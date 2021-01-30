564704d2c0e9ea1d49562decb14b301d27ac16b2
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-01-30 09:17:27 -0500

    Add lectures on scope an hoisting

--- script.js
+++ script.js
@@ -1,30 +1,79 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName} you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName} you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear < 1996) {
-      const firstName = 'Steven';
-      var millenial = true;
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear < 1996) {
+//       const firstName = 'Steven';
+//       var millenial = true;
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-      output = 'NEW OUTPUT';
-    }
-    console.log(millenial);
-    // console.log(add(2, 3));
-    console.log(output);
-  }
-  printAge();
-  return age;
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       output = 'NEW OUTPUT';
+//     }
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+/* 
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+//  Functions
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
+const addExpr = function (a, b) {
+  // used const so will be stuck in the TMZ
+  return a + b;
+};
+
+var addArrow = (a, b) => {
+  // will be undefined as it uses var
+  return a + b;
+}; */
+
+/* // Example
+if (!numProducts) deleteShoppingCart(); // will trigger deleteShoppingCart as numProducts which is undefind
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+ */
+
+// This keyword
